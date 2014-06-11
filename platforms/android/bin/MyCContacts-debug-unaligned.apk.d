ddf8a783bc9044f7a70f627d568277d6069688d4
07120809 <184765534@qq.com>
2014-06-11 15:25:10 +0800

    no

---
+++ platforms/android/bin/MyCContacts-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\contacts\platforms\android\bin\MyCContacts-debug-unaligned.apk : \
+C:\contacts\platforms\android\bin\MyCContacts.ap_ \
+C:\contacts\platforms\android\bin\classes.dex \
+C:\contacts\platforms\android\CordovaLib\bin\classes.jar \
---
+++ platforms/android/gen/cn/edu/gdmec/x07120809/contacts/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.edu.gdmec.x07120809.contacts;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
